752018335dfb5ccfc635a486025fb4f7d3d3a4c3
Sergiy Ignatenko <grydinywka@gmail.com>
2016-07-23 16:57:17 +0300

    fix fb auth locally and set bg-color for msgs

--- board/settings.py
+++ board/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd2jwp=6^d80_#-m_*g(x^d_z(0!&3f^&p&ge!6^&s85%q9us&p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
